3439b37b7564c5f4d4d17488394a59bfbd9d39df
Carlos <carlos@nextdoc.com.br>
2019-11-03 00:07:41 -0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:loginpageui/Helpers/LocaleHelper.dart';
 import 'package:loginpageui/localizations.dart';
 
 import 'pages/login.page.dart';
 
 void main() => runApp(LoginPageAppLocalization());
 
-typedef void LocaleChangeCallback(Locale locale);
-
 class LoginPageAppLocalization extends StatefulWidget {
-  
-  LocaleChangeCallback callback;
 
   @override
   _LoginPageAppLocalizationState createState() => _LoginPageAppLocalizationState();
@@ -21,7 +18,8 @@ class _LoginPageAppLocalizationState extends State<LoginPageAppLocalization> {
 
   @override void initState() {
     super.initState();
-    _specificLocalizationDelegate = SpecificLocalizationDelegate(new Locale("en"));
+    helper.onLocaleChanged = onLocaleChange;
+    _specificLocalizationDelegate = SpecificLocalizationDelegate(new Locale("pt"));
   }
 
   onLocaleChange(Locale locale) {
@@ -46,13 +44,12 @@ class _LoginPageAppLocalizationState extends State<LoginPageAppLocalization> {
       locale: _specificLocalizationDelegate.overriddenLocale,
       supportedLocales: [
         const Locale('en', ''),
-        const Locale('pt', ''),
-        const Locale.fromSubtags(languageCode: 'pt', countryCode: 'BR'), // Chinese *See Advanced Locales below*
+        const Locale('pt', '')
       ],
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginPage(),
+      home: LoginPage(title: "Flutter Login UI I18n")
     );    
   }
 }
